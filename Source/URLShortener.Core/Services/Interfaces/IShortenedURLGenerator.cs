a24d1c0b0362e2eaca54d731d3c382b4613db8ee
Michael <mejevin@gmail.com>
2021-07-14 22:34:17 +0300

    add: IShortenedURLGenerator

---
+++ Source/URLShortener.Core/Services/Interfaces/IShortenedURLGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace URLShortener.Core.Services.Interfaces
+{
+    public interface IShortenedURLGenerator
+    {
+        Uri Generate();
+    }
+}
