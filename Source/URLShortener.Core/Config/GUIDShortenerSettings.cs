5c2190e39fe2c2aea856502d8d505b314b43b3f2
Michael <mejevin@gmail.com>
2021-07-14 22:35:30 +0300

    add: settings classes for MongoDB and GUIDShortener

---
+++ Source/URLShortener.Core/Config/GUIDShortenerSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace URLShortener.Core.Config
+{
+    public class GUIDShortenerSettings
+    {
+        /// <summary>
+        /// Base path when generating a shortened URL
+        /// In form of:
+        ///     https://localhost:5000/go/
+        /// </summary>
+        public string BasePath { get; set; }
+    }
+}
---
+++ Source/URLShortener.Core/Config/MongoDBSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace URLShortener.Core.DAL
+{
+    public class MongoDBSettings
+    {
+        public string URLsCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
