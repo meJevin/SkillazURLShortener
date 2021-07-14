704101fed61c5f7ee0b7bf62919901f1f5ddf150
Michael <mejevin@gmail.com>
2021-07-14 22:32:37 +0300

    add: URL model

---
+++ Source/URLShortener.Core/Models/URL.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace URLShortener.Core.Models
+{
+    /// <summary>
+    /// Модель описывающая ссылку в БД
+    /// </summary>
+    public class URL
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Shortened { get; set; }
+        public string Original { get; set; }
+        public int ViewCount { get; set; }
+    }
+}
