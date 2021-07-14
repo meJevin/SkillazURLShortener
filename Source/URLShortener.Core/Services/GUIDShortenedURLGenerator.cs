f227e441f48671ff989376bd8fa96fb8c1e2f62d
Michael <mejevin@gmail.com>
2021-07-14 22:36:24 +0300

    add: implementation of IShortenedURLGenerator via GUID generation

---
+++ Source/URLShortener.Core/Services/GUIDShortenedURLGenerator.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using URLShortener.Core.Services.Interfaces;
+
+namespace URLShortener.Core.Services
+{
+    public class GUIDShortenedURLGenerator : IShortenedURLGenerator
+    {
+        readonly GUIDShortenerSettings _settings;
+
+        public GUIDShortenedURLGenerator(IOptions<GUIDShortenerSettings> options)
+        {
+            _settings = options.Value;
+        }
+
+        public Uri Generate()
+        {
+            return new Uri(_settings.BasePath + Guid.NewGuid().ToString().Split('-')[0]);
+        }
+    }
+
+    public class GUIDShortenerSettings
+    {
+        /// <summary>
+        /// Base path when generating a shortened URL
+        /// In form of:
+        ///     https://localhost:5000/go/
+        /// </summary>
+        public string BasePath { get; set; }
+    }
+}
