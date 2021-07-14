a3cd2029b5f3c9985c489fb7bad2d0675767bb2b
Michael <mejevin@gmail.com>
2021-07-14 22:41:08 +0300

    feat: Go controller which is responsible for redirecting the browser

---
+++ Source/URLShortener.API/Controllers/GoController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using URLShortener.Core.Config;
+using URLShortener.Core.Services;
+using URLShortener.Core.Services.Interfaces;
+
+namespace URLShortener.API.Controllers
+{
+    /// <summary>
+    /// This controller doesn't do anything, just performs an actual redirect
+    /// 
+    /// Views increase in the <see cref="URLController"/>
+    /// </summary>
+    public class GoController : ControllerBase
+    {
+        readonly IURLRepository _repo;
+        readonly GUIDShortenerSettings _settings;
+
+        public GoController(IURLRepository repository,
+            IOptions<GUIDShortenerSettings> options)
+        {
+            _repo = repository;
+            _settings = options.Value;
+        }
+
+        [HttpGet("go/{shortURL}")]
+        public IActionResult Get(string shortURL)
+        {
+            if (shortURL == null)
+            {
+                return LocalRedirect("/swagger/index.html");
+            }
+
+            var result = _repo.FindByShortened(_settings.BasePath + shortURL);
+
+            if (result == null)
+            {
+                return LocalRedirect("/swagger/index.html");
+            }
+
+            return Redirect(result.Original);
+        }
+    }
+}
