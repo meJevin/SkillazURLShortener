441cdf8f462875a98dabe32847ea5b0d5fa95074
Michael <mejevin@gmail.com>
2021-07-14 22:32:45 +0300

    add: IURLRepository

---
+++ Source/URLShortener.Core/Services/Interfaces/IURLRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using URLShortener.Core.Models;
+
+namespace URLShortener.Core.Services.Interfaces
+{
+    public interface IURLRepository
+    {
+        URL Save(URL toSave);
+        URL Update(URL toUpdate);
+        IEnumerable<URL> GetAll();
+        URL FindByShortened(string shortened);
+    }
+}
